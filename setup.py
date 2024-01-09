7637267fd269e2a5d7e7f1808a732082465d3313
T0kyoB0y <kparra2023@alu.uct.cl>
2024-01-08 21:40:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="PotatoWidgets",
-    version="1.0.6",
+    version="1.1.6",
     packages=find_packages(),
     install_requires=["PyGObject"],
     entry_points={
