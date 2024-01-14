2a957bad5ae7a9f63aadd5ba6383bb48cb8e0024
T0kyoB0y <kparra2023@alu.uct.cl>
2024-01-13 21:40:04 -0300

    owo

--- PotatoWidgets/__Import.py
+++ PotatoWidgets/__Import.py
@@ -7,6 +7,9 @@ import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
 gi.require_version("GtkLayerShell", "0.1")
+gi.require_version("GdkPixbuf", "2.0")
+gi.require_version("GObject", "2.0")
+
 
 from gi.repository import (Gdk, GdkPixbuf, Gio, GLib, GObject, Gtk,
                            GtkLayerShell)
