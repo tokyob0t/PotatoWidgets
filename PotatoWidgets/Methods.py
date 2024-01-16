2a4656e22b05a8b8a4cf8c66226f167e7ae92ced
T0kyoB0y <kparra2023@alu.uct.cl>
2024-01-16 17:55:55 -0300

    Added wait method

---
+++ PotatoWidgets/Methods.py
@@ -0,0 +1,11 @@
+from .__Import import *
+
+
+def wait(time_ms, callback):
+    def on_timeout():
+        callback()
+        return False
+
+    GLib.timeout_add(time_ms, on_timeout)
+
+    GLib.idle_add(lambda: None)
