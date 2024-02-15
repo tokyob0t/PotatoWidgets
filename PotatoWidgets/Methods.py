bc1a3bcf1585975e417cd2a9f2102285c9b219ab
T0kyoB0y <kparra2023@alu.uct.cl>
2024-02-14 21:27:41 -0300

    Updated lookup_icon method, also updated Image widget

--- PotatoWidgets/Methods.py
+++ PotatoWidgets/Methods.py
@@ -2,23 +2,28 @@ from .Imports import *
 from .Variable import Poll
 
 
-def wait(time_ms, callback):
+def wait(time_ms: Union[str, int], callback: Callable) -> None:
     def on_timeout():
         callback()
         return False
 
-    return GLib.timeout_add(Poll._parse_interval(time_ms), on_timeout)
+    GLib.timeout_add(Poll._parse_interval(time_ms), on_timeout)
 
 
-def lookup_icon(icon_name, size=128, path=True):
+def lookup_icon(
+    icon_name: str,
+    size: Literal[8, 16, 32, 64, 128] = 128,
+    path: bool = True,
+    fallback: str = "application-x-addon-symbolic",
+) -> str:
     if icon_name is not None:
         theme = Gtk.IconTheme.get_default()
 
         for name in [
+            icon_name,
             icon_name.lower(),
             icon_name.title(),
             icon_name.capitalize(),
-            icon_name,
         ]:
             icon_info = theme.lookup_icon(
                 name,
@@ -28,4 +33,4 @@ def lookup_icon(icon_name, size=128, path=True):
             if icon_info is not None:
                 return icon_info.get_filename() if path else icon_info
 
-    return "" if path else lookup_icon("application-x-addon-symbolic")
+    return "" if path else lookup_icon(fallback)
