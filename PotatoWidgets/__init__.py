2e8aa910fa67d0bf359fa51a843bfb9991015c97
T0kyoB0y <kparra2023@alu.uct.cl>
2024-02-18 18:56:32 -0300

    added parse_interval method

--- PotatoWidgets/__init__.py
+++ PotatoWidgets/__init__.py
@@ -1,6 +1,6 @@
 from . import Widget
 from .Imports import Gdk, GdkPixbuf, Gio, GLib, GObject, Gtk, GtkLayerShell, Pango
-from .Methods import wait, lookup_icon
+from .Methods import wait, lookup_icon, parse_interval
 from .PotatoLoop import PotatoLoop
 from .Style import Style
 from .Variable import Listener, Poll, Variable
