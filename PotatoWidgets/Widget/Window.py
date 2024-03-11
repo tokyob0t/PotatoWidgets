a92633179b6842080cb93c949c13221ac6eabb56
T0kyoB0y <kparra2023@alu.uct.cl>
2024-03-11 12:41:10 -0300

    Fix set_size method for window

--- PotatoWidgets/Widget/Window.py
+++ PotatoWidgets/Widget/Window.py
@@ -6,7 +6,7 @@ from ..Variable import Listener, Poll, Variable
 class Window(Gtk.Window):
     def __init__(
         self,
-        size: Union[int, str, List[Union[int, str]], List[int]] = 0,
+        size: List[Union[str, int]] = [0, 0],
         at: dict = {},
         position: str = "center",
         layer: str = "top",
