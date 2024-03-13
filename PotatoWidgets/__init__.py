1fa9aee59f7540f0e8f777c64eae75c9fb72c3b2
T0kyoB0y <kparra2023@alu.uct.cl>
2024-03-13 18:46:47 -0300

    Updated Imports

--- PotatoWidgets/__init__.py
+++ PotatoWidgets/__init__.py
@@ -3,4 +3,5 @@ from .Env import *
 from .Methods import (get_screen_size, lookup_icon, parse_interval,
                       parse_screen_size, wait)
 from .PotatoLoop import PotatoLoop
+from .Services import Applications, BatteryService, NotificationsService
 from .Variable import Listener, Poll, Variable
