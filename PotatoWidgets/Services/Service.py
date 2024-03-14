342c03e12003b5a742b0702e8f03ff08b300463e
T0kyoB0y <kparra2023@alu.uct.cl>
2024-03-13 21:51:08 -0300

    Updated Services

--- PotatoWidgets/Services/Service.py
+++ PotatoWidgets/Services/Service.py
@@ -36,8 +36,8 @@ class Service(GObject.Object):
 
 
 class ServiceChildren(Service):
-    def __new__(cls):
-        pass
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
 
     def __init__(self) -> None:
         super().__init__()
