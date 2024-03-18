597316b5cea5a7872667f4d360cdce56b45c09fe
T0kyoB0y <kparra2023@alu.uct.cl>
2024-03-18 12:38:37 -0300

    Updated Potatocli

--- PotatoWidgets/Cli/__main__.py
+++ PotatoWidgets/Cli/__main__.py
@@ -34,10 +34,20 @@ def list_functions(iface):
         print(f"Error listing functions: {e}")
 
 
+def list_variables(iface):
+    try:
+        variables: List[str] = iface.ListVariables()
+        if variables:
+            for v in variables:
+                print(v)
+    except dbus.exceptions.DBusException as e:
+        print(f"Error listing functions: {e}")
+
+
 def exec_function(iface, func_name):
     try:
-        out: str = iface.ExecFunction(func_name)
-        print(out)
+        response: str = iface.ExecFunction(func_name)
+        print(response)
 
     except dbus.exceptions.DBusException as e:
         print(f"Error while executing the callback: {e}")
@@ -45,30 +55,34 @@ def exec_function(iface, func_name):
 
 def window_action(iface, action, window_name):
     try:
-        out: str = iface.WindowAction(action, window_name)
-        print(out)
+        response: str = iface.WindowAction(action, window_name)
+        print(response)
     except dbus.exceptions.DBusException as e:
         print(f"Error toggling window: {e}")
 
 
 def main():
+
     parser = argparse.ArgumentParser(description="PotatoWidgets CLI")
 
-    parser.add_argument(
-        "--windows", action="store_true", help="List all exported windows"
-    )
+    args_withoutmetavar: List[List[str]] = [
+        ["--windows", "List all exported windows"],
+        ["--functions", "List all exported functions"],
+        ["--variables", "List all exported variables"],
+    ]
 
-    parser.add_argument(
-        "--functions", action="store_true", help="List all exported functions"
-    )
+    args_withmetavar: List[List[str]] = [
+        ["--exec", "<FUNCTION>", "Execute an exported function"],
+        ["--open", "<WINDOW>", "Open a window"],
+        ["--close", "<WINDOW>", "Close a window"],
+        ["--toggle", "<WINDOW>", "Toggle a window"],
+    ]
 
-    parser.add_argument(
-        "--exec", metavar="<FUNCTION>", help="Execute an exported function"
-    )
+    for i in args_withmetavar:
+        parser.add_argument(i[0], metavar=i[1], help=i[2])
 
-    parser.add_argument("--open", metavar="<WINDOW>", help="Open a window")
-    parser.add_argument("--close", metavar="<WINDOW>", help="Close a window ")
-    parser.add_argument("--toggle", metavar="<WINDOW>", help="Toggle window")
+    for i in args_withoutmetavar:
+        parser.add_argument(i[0], action="store_true", help=i[1])
 
     args = parser.parse_args()
 
@@ -78,6 +92,9 @@ def main():
     elif args.functions:
         iface = get_iface()
         list_functions(iface)
+    if args.variables:
+        iface = get_iface()
+        list_variables(iface)
     elif args.exec:
         iface = get_iface()
         exec_function(iface, args.exec)
