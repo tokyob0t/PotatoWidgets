9843ce3b8aa868d394b4d84aa461ee27213a627f
T0kyoB0y <kparra2023@alu.uct.cl>
2024-03-30 20:48:03 -0300

    Fix imports

--- PotatoWidgets/__init__.py
+++ PotatoWidgets/__init__.py
@@ -2,11 +2,29 @@
 Welcome to PotatoWidgets :D
 """
 
-__all__ = ["Widget", "Bash", "PotatoLoop", "Listener", "Poll", "Variable"]
+__all__ = [
+    "Widget",
+    "Bash",
+    "PotatoLoop",
+    "Listener",
+    "Poll",
+    "Variable",
+    "Gdk",
+    "GdkPixbuf",
+    "Gio",
+    "GObject",
+    "Gtk",
+    "GtkLayerShell",
+    "Pango",
+    "Playerctl",
+    "GLib",
+]
 
 from . import Widget
 from .Bash import *
 from .Env import *
+from .Imports import (Gdk, GdkPixbuf, Gio, GLib, GObject, Gtk, GtkLayerShell,
+                      Pango, Playerctl)
 from .Methods import *
 from .PotatoLoop import *
 from .Services import *
